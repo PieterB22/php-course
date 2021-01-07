ecd6b0de699c53b093cee4dcbcbeb86917bf728e
Pieter <pieter@iedm.co.za>
2021-01-07 15:40:12 +0200

    php course conclusion

--- index.php
+++ index.php
@@ -14,36 +14,34 @@
     $products['iPhone']=10000;
     $products['Toaster']=750;
 
-    //Math variables
-    $amount=8000;
-    $taxRate=0.0825;
-    $addedTax=$amount*$taxRate;
-
-    echo $addedTax;
+    $taxRate=0.825;
 
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have R".$credit." in your wallet.</h2>";
 
+    //Tax calculation function
+    function tax_calc($amount,$tax){
+      $calculate_tax = $amount*$tax;
+      $amountWithTax=round($amount+$calculate_tax,2);
+      return $amountWithTax;
+    }
+
     // Show the price of all the products
     foreach($products as $key => $value){
-      echo "<p>The ".$key." costs R".$value."</p>";
+      $costWithTax = tax_calc($value,$taxRate);
+      echo "<p>The ".$key." costs R".$costWithTax." with tax</p>";
     }
 
     //Products the viewer can afford
     echo "<h2>Items you can afford</h2>";
+
     foreach($products as $key => $value){
-      if($value <= $credit){
-        echo "<p>".$key."</p>";
+      $costWithTax = tax_calc($value,$taxRate);
+      if($costWithTax <= $credit ){
+        echo "<p>".$key."</p>"; 
       }
     }
 
-    //Tax calculation function
-    function tax_calc($amount,$tax){
-      $calculate_tax = $amount*$tax;
-      $amount=round($amount+$calculate_tax,2);
-      return $amount;
-    }
-    echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
