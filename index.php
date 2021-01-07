3c493c3b8afa820878d39c497c0c5ee96ae1df9b
Pieter <pieter@iedm.co.za>
2021-01-07 15:05:08 +0200

    Array Variables

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
     <?php
     $name = "PHP Test Store";
     $credit = 10000;
+    $products['Computer']=7500;
+    $products['Car']=150000;
+    $products['iPhone']=10000;
+    $products['Toaster']=750;
 
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have R".$credit." in your wallet.</h2>";
+
+    echo "<p>A car costs R".$products['Car']."</p>"
     ?>
   </body>
 </html>
